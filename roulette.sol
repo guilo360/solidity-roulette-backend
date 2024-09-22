a0ef5ee2962de94f84b91b65a47047c0a1230552
guilo360 <52168555+guilo360@users.noreply.github.com>
2024-09-22 13:52:05 +1200

    Update roulette.sol

--- roulette.sol
+++ roulette.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "rng.sol";
+
+contract rouletteWheel {
+
+    // Sets the range of numbers on the wheel
+    uint wheelSize;
+
+    //holds what colour each number (represented by an index value) is
+    //I think this is more memory eficient than a mapping?
+    string[] wheelcolour;
+
+    constructor() {
+        wheelSize = 37;
+        wheelcolour[0] = "green";
+        
+        //initialises odd numbers to be black
+        for (uint i=1; i<=wheelSize; i = i +2) 
+        {
+            wheelcolour[i] = "black";
+        }
+
+        //initialises even numbers to be red
+        for (uint i=2; i<=wheelSize; i = i +2) 
+        {
+            wheelcolour[i] = "red";
+        }
+    }
+
+}
