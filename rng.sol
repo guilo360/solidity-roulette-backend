b798318f6300f961c7e596291061a0620f6267b1
guilo360 <52168555+guilo360@users.noreply.github.com>
2024-09-22 13:24:57 +1200

    Update rng.sol

--- rng.sol
+++ rng.sol
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract random {
-    uint seed;
-    uint random;
+//this contract is a library of random number functions that will be used by the roulette contract
 
-    function rand() public 
-    returns (uint rand)
+contract random {
+
+//This function only gets the current blocks hash, and is an insecure random hash generator
+
+    function randHash() public view
+    returns (uint _randomhash)
     {
-        seed = block.timestamp;
-        uint randomhash = uint256(bytes32(keccak256(abi.encodePacked(seed))));
-        return randomhash;
+        uint seed = block.timestamp;
+        return uint256(bytes32(keccak256(abi.encodePacked(seed))));
+
+    }
+
+//turns the above hash function into a random number within the range (high) and (low)
+//This is implimented this way so that alternative random hash functions can be implimented without breaking other code
+//note - due to the nature of the blockchain any waiting till a new block has been mined should be handeled by the front end
+
+
+    function randNumber(uint _low, uint _high) public view
+    returns (uint randomInRange) 
+    {
+        uint hash = randHash();
+        return hash % (_high + 1) + _low;
     }
 
 }
